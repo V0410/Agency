3cc0272ae3c1ec8591672d754e0a69f21d8c442f
capybara003 <mantoketo4+1@gmail.com>
2025-03-14 12:33:36 -0700

    Update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -55,14 +55,14 @@ const Header = () => {
                   } `}
               >
                 <Image
-                  src="/images/logo/logo.webp"
+                  src="/images/logo/logo-sixcool.svg"
                   alt="logo"
                   width={140}
                   height={30}
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/logo.webp"
+                  src="/images/logo/logo-sixcool.svg"
                   alt="logo"
                   width={140}
                   height={30}
